29d4e31b01ea0cb6fc6710948fb12859dbc0e0c0
Vasu Nurukurthi <48316372+Whitedevil6@users.noreply.github.com>
2020-05-19 22:45:14 +0530

    Add files via upload

---
+++ Concatenate .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s1[1000 ], s2[1000 ];
+    printf("entew str :");
+    scanf("%s",s1);
+    printf(" string 2 : " );
+    scanf("%s", s2);
+    printf(" concatenate.. %s", strcat(s1, s2));
+    
+
+}
+    
+    

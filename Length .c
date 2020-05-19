003e999cf1caa92dc33799f8d1ab75378c5c8e58
Vasu Nurukurthi <48316372+Whitedevil6@users.noreply.github.com>
2020-05-19 22:45:37 +0530

    Add files via upload

---
+++ Length .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int len=0, i; char s[100];
+    printf("entew str :");
+    scanf("%s",s);
+    for(i=0;s[i]!='\0';i++)
+    len++;
+    printf(" concatenate.. %d",len);
+    
+
+}
+    
+    
